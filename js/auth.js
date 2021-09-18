b1d03041e64de1648b41242195f1281d69203909
Caio Menezes <caio.menezes@fairycode.com.br>
2021-09-17 21:53:35 -0300

    Implementação da função de register

--- js/auth.js
+++ js/auth.js
@@ -8,10 +8,27 @@ function login() {
             .auth()
             .signInWithEmailAndPassword(email,password)
             .then( () => {
-                alert('Logado')
+                alert('Usuário logado com sucesso, Bem vindo!')
                 window.location.href = "index.html"
             })
             .catch((error) => {
                 alert(error)
             })
 }
+
+function register() {
+    const email = document.getElementById('email').value
+    const password = document.getElementById('password').value
+    firebase
+    .auth()
+    .createUserWithEmailAndPassword(email,password)
+    .then(() => {
+        alert('usuário registrado com sucesso, efetue o login para continuar');
+        window.location.href = "login.html"
+    })
+    .catch((error) => {
+        alert(error)
+    })
+}
+
+    
