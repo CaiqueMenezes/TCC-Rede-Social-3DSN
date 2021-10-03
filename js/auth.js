d59ac982a6d221bae153091b68bb6a88441ec563
Caio Menezes <caio.menezes@fairycode.com.br>
2021-10-03 11:35:51 -0300

    Updates gerais/login com google

--- js/auth.js
+++ js/auth.js
@@ -1,5 +1,7 @@
 auth = firebase.auth()
 
+
+
 function login() {
     if (firebase.auth().currentUser) {
         firebase.auth().signOut()
@@ -20,19 +22,44 @@ function login() {
 
 }
 
+const googleLogin = () => {
+    var provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth()
+        .signInWithPopup(provider)
+        .then((result) => {
+            var credential = result.credential;
+
+            // This gives you a Google Access Token. You can use it to access the Google API.
+            var token = credential.accessToken;
+            // The signed-in user info.
+            var user = result.user;
+            // ...
+        }).catch((error) => {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            // The email of the user's account used.
+            var email = error.email;
+            // The firebase.auth.AuthCredential type that was used.
+            var credential = error.credential;
+            console.log(error)
+        });
+}
+
 firebase.auth().onAuthStateChanged(user => {
     if (user) {
-      var uid = user.uid;
-      
-      
-  } else {
-    if (window.location.pathname === '/register.html') return // user is in login page, skip.
-    if (window.location.pathname !== '/login.html') {
-      window.location = 'login.html'
+        var uid = user.uid;
+
+    } else {
+        if (window.location.pathname === '/register.html') return // user is in login page, skip.
+        if (window.location.pathname !== '/login.html') return
+        if (window.location.pathname == 'index.html') return
+        if (window.location.pathname == '/projetos-cadastrados') {
+            window.location = 'login.html'
+        }
     }
-   }
-  })
-    
+})
+
 
 
 function register() {
@@ -53,15 +80,15 @@ function register() {
 
 function logOut() {
     auth.signOut()
-    .then(() => {
-        alert('Usuário deslogado com sucesso');
-        window.location.href = "login.html"
-    })
-    .catch((error) => {
-        alert(error)
-    })
+        .then(() => {
+            alert('Usuário deslogado com sucesso');
+            window.location.href = "login.html"
+        })
+        .catch((error) => {
+            alert(error)
+        })
 
-   
+
 }
 
 function funcoes() {
