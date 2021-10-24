cd1897281b9a10efc487be20c897a21bd8a8c68e
Caio Menezes <caio.menezes@fairycode.com.br>
2021-10-24 18:00:40 -0300

    teste

--- js/ongRegister.js
+++ js/ongRegister.js
@@ -1,4 +1,24 @@
-const axios = require('axios')
+function Cadastrar() {
+    let name = document.getElementById('name').value
+    let description = document.getElementById('description').value
+    let cause = document.getElementById('cause').value
+    let contact = document.getElementById('contact').value
+    let email = document.getElementById('email').value
+    let address = document.getElementById('address').value
+
+    axios.post("http://localhost:3001/ongRegister", {
+        'name': name,
+        'description': description,
+        'cause': cause,
+        'contact': contact,
+        'email': email,
+        'address': address
+    }).then((res) => {
+        console.log(res)
+    }).catch((err) => {
+        console.log(err)
+    })
+}
 
 
 
